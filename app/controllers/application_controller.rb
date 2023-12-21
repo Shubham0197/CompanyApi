768e55485b0cd42d6ee827d0fe73e2273e9e8d23
shubhamjain0197 <shubham.jain@itsacheckmate.com>
2023-12-21 15:26:35 +0530

    authenticate user based on the jwt token in request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+
+  def authenticate_user!
+    head :unauthorized unless current_user
+  end
+
+  def current_user
+    @current_user ||= decode_jwt_token
+  end
+
+  def decode_jwt_token
+    token = request.headers['Authorization']&.split&.last
+    return unless token
+
+    begin
+      # Assuming you are using the 'jwt' gem
+      payload = JWT.decode(token, 'SECRET_KEY_BASE', true).first
+      User.find(payload['user_id'])
+    rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+      nil
+    end
+  end
+
 end
